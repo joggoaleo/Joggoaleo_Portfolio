927d0f87cad1d048be7e506d0747f85965c0987d
joggoaleo <sunjihoon00@naver.com>
2022-09-21 19:51:39 +0900

    add language skill button

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-function revealMyinfo(myInfo, className) {
+function revealMyInfo(myInfo, className, color) {
   let htmlElement = document.querySelector(className);
-  
+
   htmlElement.innerHTML = myInfo;
   htmlElement.style.color = color;
 }
-
-/* function revealMyname1(myName) {
-  let span = document.getElementsByTagName("span");
-  span[1].innerHTML = myName;
-} */
